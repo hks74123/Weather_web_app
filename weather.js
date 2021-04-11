cc9ee6dad142aacfc66766fbbbbc366556cba3a6
hks_74123 <hemant91852@gmail.com>
2021-04-11 17:12:47 +0530

    first commit

---
+++ weather.js
@@ -0,0 +1,36 @@
+
+if (navigator.geolocation) {
+	navigator.geolocation.getCurrentPosition(weatherBallon);
+}
+function weatherBallon(position) {
+	var lat=position.coords.latitude;
+	var lon=position.coords.longitude;
+	var key = sessionStorage.getItem("ss")
+	fetch('https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&appid='+key)  
+	.then(function(resp) { return resp.json() }) // Convert data to json
+	.then(function(data) {
+		drawWeather(data);
+	})
+	.catch(function() {
+		// catch any errors
+	});
+}
+function drawWeather( d ) {
+  var celcius = Math.round(parseFloat(d.main.temp)-273.15);
+	var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32);
+  var description = d.weather[0].description; 
+	
+	document.getElementById('description').innerHTML = description;
+	document.getElementById('temp').innerHTML = celcius + '&deg;';
+	document.getElementById('location').innerHTML = d.name;
+  
+  if( description.indexOf('rain') > 0 ) {
+  	document.body.className = 'rainy';
+  } else if( description.indexOf('cloud') > 0 ) {
+  	document.body.className = 'cloudy';
+  } else if( description.indexOf('sunny') > 0 ) {
+  	document.body.className = 'sunny';
+  } else {
+  	document.body.className = 'clear';
+  }
+}
